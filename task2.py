7d0100c73f33dc7fbc4049ca85ef3763a6b8e886
arshi8294 <arshi829495@gmail.com>
2024-03-01 15:26:12 +0330

    Compeleting task2

--- task2.py
+++ task2.py
@@ -2,9 +2,10 @@ bankAccount = {"Account owner" : "Arshia Amoozad", "Account ID" : "1234567", "Ac
 
 commands = [0, 1, 2, 3]
 
+print("---Welcome to your Bank Account---")
+
 while True:
     
-    print("---Welcome to your Bank Account---")
     print("Choose which service you want to use :")
     command = int(input("1-Account balance \n2-Cash withdrawal \n3-Deposit cash \n0-Quit \n"))
     
@@ -12,9 +13,48 @@ while True:
     while command not in commands:
         print("Invalid input. Please choose carefully.")
         print("Choose which service you want to use :")
-        command = int(input("1-Account balance \n2-Cash withdrawal \n3-Deposit cash \n0-Quit \n"))
+        command = int(input("1-Account balance \n2-Withdrawal money \n3-Deposit cash \n0-Quit \n"))
 
     if command == 0:
+        print("Thank you for choosing us")
+        break
+
+    elif command == 1 :
+        print("-----------------------------------------------------")
+        print(f"Account ID : {bankAccount['Account ID']}\nAccount owner : {bankAccount['Account owner']}\nAccount balance : {bankAccount['Account balance']}$")
+        print("-----------------------------------------------------")
+
+
+    elif command == 2:
+        print("-----------------------------------------------------")
+        
+        amount = float(input("Enter your desired amount to withdrawal : "))
+
+        if amount > bankAccount["Account balance"]:
+            print("Your account balance is insufficient")
+    
+        else :
+            bankAccount["Account balance"] -= amount
+            print(f"Operation done sucssesfully\nYour account balance is {bankAccount["Account balance"]}")
+        print("-----------------------------------------------------")
+        
+    
+    else :
+        print("-----------------------------------------------------")
+        amount = float(input("Enter your desired amount to deposit : "))
+        bankAccount["Account balance"] += amount
+        print("Operation done sucssesfully", f"Your account balance is {bankAccount["Account balance"]}")
+        print("-----------------------------------------------------")
+    
+    end = int(input("Do you want to continue? \n1-Yes\n0-No  \n"))
+    if not end:
+        print("Thank you for choosing us")
         break
     
+
+
+        
+
+
+
     
