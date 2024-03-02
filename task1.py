318ecc90f668713ceff895efa4673f1a9d655498
arshi8294 <arshi829495@gmail.com>
2024-03-03 00:07:07 +0330

    implicit task

---
+++ task1.py
@@ -0,0 +1,9 @@
+userInput = input("Enter your data : \n")
+
+
+
+
+
+       
+# This task is very unclear and need more info 
+# for now I can only use try/except to convert string to int, float, or leave it as str if both of them didn't work
