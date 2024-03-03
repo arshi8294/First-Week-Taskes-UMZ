4ed231398bd3563a2812704b94ca51b8810c62e3
arshi8294 <arshi829495@gmail.com>
2024-03-04 00:00:42 +0330

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,41 @@
-userInput = input("Enter your data : \n")
+def isList(x:str):
+    if "," in x:
+        return True
+    return False
+    
+
+def isFloat(x:str):
+    dataBase = [str(i) for i in range(10)]
+    dataBase.append(".")
+    for i in x:
+        if i in dataBase:
+            pass
+        else:
+            return False
+    return True
 
 
+def isInt(x:str):
+    dataBase = [str(i) for i in range(10)]
+    for i in x:
+        if i in dataBase:
+            pass
+        else:
+            return False
+    return True
 
+userInput = input("Enter your data : ")
 
+if isList(userInput):
+    lst = userInput.split(",")
+    lst = [i.strip() for i in lst]
+    print(f"Your input is list items and it equals to : {lst} ")
+elif isInt(userInput): 
+    iNum = int(userInput)
+    print(f"Your input is an integer number and it equals to : {iNum}")
+elif isFloat(userInput):
+    fNum = float(userInput)
+    print(f"Your input is a float number and it equals to : {fNum}")
+else :
+    print(f"Your input is a normal string and it equals to : {userInput}")
 
-       
-# This task is very unclear and need more info 
-# for now I can only use try/except to convert string to int, float, or leave it as str if both of them didn't work
